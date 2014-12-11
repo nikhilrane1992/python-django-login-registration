246ffe89e2138bf760a33f0c57a18e5edf7fdfe4
Nikhil Suresh Rane <nikhilrane1992@gmail.com>
2014-12-11 15:17:31 +0530

    Register successfully using android app

--- django_login_system/django_login_system/views.py
+++ django_login_system/django_login_system/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render_to_response, HttpResponseRedirect
 from django.contrib import auth
 import json
 from forms import AdminRegistrationForm
+from django.contrib.auth.models import User
 
 
 #------------------------------------------------------------------------------------------------------------------------
@@ -63,23 +64,23 @@ def auth_view(request):
 #                             Register new user using post request
 #------------------------------------------------------------------------------------------------------------------------
 def registerUser(request):
-    if request.POST:
-        dataDictionary = json.loads(request.body)
-        username = request.POST['username']
-        email = request.POST['email']
-        password = request.POST['password']
-        password1 = request.POST['password1']
-
-        user=User()
-        user.username = username
-        user.email = email
-        if password == password1:
-            user.set_password(password)
-        else:
-            return HttpResponse(json.dumps([{"validation": "Password does not match", "status": False}]), content_type="application/json")
-        # user.is_staff = True
-        user.save()
-        register_success = [{"validation" : "Registration Successful", "status": True}]
-        return HttpResponse(json.dumps(register_success), content_type="application/json")
-    else:
-        return HttpResponse(json.dumps([{"validation": "I am watching you (0_0)", "status": False}]), content_type="application/json")
+	# print request.POST
+	if request.POST:
+		username = request.POST['username']
+		email = request.POST['email']
+		password = request.POST['password']
+		password1 = request.POST['password1']
+		print username, email, password1, password
+		user=User()
+		user.username = username
+		user.email = email
+		if password == password1:
+			user.set_password(password)
+		else:
+			return HttpResponse(json.dumps([{"validation": "Password does not match", "status": False}]), content_type="application/json")
+		# user.is_staff = True
+		user.save()
+		register_success = [{"validation" : "Registration Successful", "status": True}]
+		return HttpResponse(json.dumps(register_success), content_type="application/json")
+	else:
+		return HttpResponse(json.dumps([{"validation": "I am watching you (0_0)", "status": False}]), content_type="application/json")
